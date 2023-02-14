a136070d433075a8ac341fcd52ba35e37030a89e
Md Badal Hasan <120394060+badalhasan@users.noreply.github.com>
2023-02-14 01:13:10 -0300

    Add files via upload

---
+++ join.js
@@ -0,0 +1,20 @@
+let counters = document.querySelectorAll('.counter');
+let time = 300;
+
+counters.forEach(counter =>{
+    let updateCount =() => {
+    let target = +counter.getAttribute(data-target);
+    let count = +counter.innerText;
+    let  increment = target / time;
+
+    if (count < target) {
+      counter.innerText = Math.ceil(count + increment);
+      setTimeout( updateCount, 1);
+
+    } else {
+      count.innerText = target; 
+    }
+  };
+  updateCount();
+
+});
